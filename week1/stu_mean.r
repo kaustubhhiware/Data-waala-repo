fa0e7dae8c9011eee7ac5c469727a1c2f9b8893c
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-08-17 19:51:19 +0530

    na works for mean

--- week1/stu_mean.r
+++ week1/stu_mean.r
@@ -1,19 +1,21 @@
 
 getData <- function(path) {
-	table1 <- read.csv(path, header = T, sep = ",");
-	stud_mean <- matrix(apply(table1[,-1], 1, mean));
+	table1 <- read.csv(path, header = T, sep=",");
+	#tabla1 <- read.csv(path,skip=1)
+	table1$mean <- round(rowMeans(table1[,-1],na.rm=TRUE),2);
 	stud_var <- matrix(apply(table1[,-1], 1, var));
 	stud_stddev <- matrix(apply(stud_var, 1, sqrt));
 	sub_mean <- matrix(apply(table1[,-1], 2, mean));
 	sub_var <- matrix(apply(table1[,-1], 2, var));
 	sub_stddev <- matrix(apply(sub_var, 2, sqrt));
 
+	#tabla1$var <- stud_var
 	#stud_std <- apply(table1[,-1], 1, std);
-	stud_medians <- median(stud_mean);
+	stud_medians <- apply(table1,1,median,na.rm=TRUE);
 	sub_medians <- median(sub_mean);
 	print(stud_medians);
 	print(sub_medians);
-	write.table(stud_mean, file = "stu_mean.csv", sep=",", col.names=F);
+	write.table(table1, file = "stu_mean.csv", sep=",", col.names=F);
 	write.table(stud_var, file = "stu_var.csv" , sep=",", col.names=F);
 	write.table(stud_stddev, file = "stu_stddev.csv" , sep=",", col.names=F);
 	write.table(sub_mean, file = "sub_mean.csv", sep=",", col.names=F);
@@ -21,17 +23,5 @@ getData <- function(path) {
 	write.table(sub_stddev, file = "sub_stddev.csv" , sep=",", col.names=F);
 }
 
-getData("Data1.csv");
+getData("Data2.csv");
 
-
-
-# stu.var = function(path) {
-# 	table1 = read.csv(path);
-# 	stud_mean = apply(table1[-1,], 1, var);
-# 	write.csv(studmean);
-# }
-# stumean = function(path) {
-# 	table1 = read.csv(path);
-# 	stud_mean = apply(table1[-1,], 1, std);
-# 	write.csv(studmean);
-# }
