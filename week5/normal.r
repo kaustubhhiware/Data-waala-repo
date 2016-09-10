b9fe207df940dbdc79e3470c3e4d9f7aa44ebf8b
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-09-10 17:26:34 +0530

    All parts norm , binom , pois

--- week5/normal.r
+++ week5/normal.r
@@ -6,12 +6,15 @@ rowVars1 <- function (x,na.rm = TRUE)
   return(rowSums(sqr(x - rowMeans(x,na.rm = na.rm)), na.rm = na.rm)/(n - 1))
 }
 ###
-###	Results generated are stored in poisson.csv and the plots are in poisson_histogram.pdf
+###	Results generated are stored in normal.csv and the plots are in normal_histogram.pdf
 ###
-poisson.samples <- function(mean, n) {
+normal.samples <- function(mean, n) {
 	arr <- matrix(nrow = 1, ncol = n);
 	for(i in 1:n) {
-		arr[1,i] <- ppois(i-1, lambda = mean);
+	  s<-sample(20:100000,1);# generate sample size value from 20 to 1,00,000
+	  meu<-s*mean;
+	  sigma<-sqrt(meu);
+	  arr[1,i] <- pnorm(i-1, mean = meu, sd = sigma);
 	}
 	return(arr);
 }
@@ -20,22 +23,22 @@ get.samples <- function(n,rows) {
 	arr <- matrix(nrow = rows, ncol = n);
 	meanarr <- runif(rows, min = 1, max = n);
 	for(i in 1:rows) {
-		temparr <- poisson.samples(meanarr[i], n);
+		temparr <- normal.samples(meanarr[i], n);
 		for(j in 1:n) {
 			arr[i,] <- temparr[1,];
 		}
 	}
-	write.table(arr, file = "poisson.csv", sep=",", row.names = F, col.names=T);
+	write.table(arr, file = "normal.csv", sep=",", row.names = F, col.names=T);
 	
-	Data <-read.csv("poisson.csv",header = T,sep=",")
+	Data <-read.csv("normal.csv",header = T,sep=",")
 	Data$mean <- round(rowMeans(Data[,-1],na.rm=TRUE),4);
 	Data$var <- round(rowVars1(Data[,-1],na.rm=TRUE),4);
   
-	pdf("poisson_histogram.pdf");
+	pdf("normal_histogram.pdf");
 	hist(Data$mean);
 	hist(Data$var);
 	dev.off();
- 	write.table(Data[,-1], file = "poisson.csv", sep=",", row.names=F, col.names=T);
+ 	write.table(Data[,-1], file = "normal.csv", sep=",", row.names=F, col.names=T);
   
 }
 get.samples(20,10000);# value of n is 20 and 10,000 samples drawn
